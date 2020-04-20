cc3756355a502e9e1c62000dce5474a0f2bbd2ff
Rajanshu Ujjwal <rajanshu.ujjwal@gmail.com>
2020-04-21 00:53:54 +0530

    todoApp-part1-noauth

---
+++ functions/APIs/todos.js
@@ -0,0 +1,95 @@
+//todos.js
+
+//adding firebase database
+const { db } = require('../util/admin');
+
+exports.getAllTodos = (request, response) => {
+    db
+        .collection('todos')
+        .orderBy('createAt', 'desc')
+        .get()
+        .then((data) => {
+            let todos = [];
+            data.forEach((doc) => {
+                todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+                    body: doc.data().body,
+                    createAt: doc.data().createAt,
+                });
+            });
+            return response.json(todos);
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({ error: err.code });
+        });
+};
+
+
+// Posting ToDo List
+exports.postOneTodo = (request, response) => {
+	if (request.body.body.trim() === '') {
+		return response.status(400).json({ body: 'Must not be empty' });
+    }
+    
+    if(request.body.title.trim() === '') {
+        return response.status(400).json({ title: 'Must not be empty' });
+    }
+    
+    const newTodoItem = {
+        title: request.body.title,
+        body: request.body.body,
+        createAt: new Date().toISOString()
+    }
+    db
+        .collection('todos')
+        .add(newTodoItem)
+        .then((doc)=>{
+            const responseTodoItem = newTodoItem;
+            responseTodoItem.id = doc.id;
+            return response.json(responseTodoItem);
+        })
+        .catch((err) => {
+			response.status(500).json({ error: 'Something went wrong' });
+			console.error(err);
+		});
+};
+
+// Deleting ToDo List
+exports.deleteTodo = (request, response) => {
+    const document = db.doc(`/todos/${request.params.todoId}`);
+    document
+        .get()
+        .then((doc) => {
+            if (!doc.exists) {
+                return response.status(404).json({error: 'Todo not found'})
+            }
+            return document.delete();
+        })
+        .then(() => {
+            response.json({ message: 'Delete success'});
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({error: err.code});
+        });
+};
+
+// Editing ToDo 
+exports.editTodo = (request, response) => {
+    if(request.body.todoId || request.body.createAt) {
+        response.status(403).json({message: 'Not allowed to edit'});
+    }
+    let document = db.collection('todos').doc(`${request.params.todoId}`);
+    document.update(request.body)
+    .then(() => {
+        response.json({message: 'Updated successfully'});
+    })
+    .catch((err) => {
+        console.error(err);
+        return response.status(500).json({
+            error: err.code
+        });
+    });
+};
---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const app = require('express')();
+
+const {
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
+exports.api = functions.https.onRequest(app);
